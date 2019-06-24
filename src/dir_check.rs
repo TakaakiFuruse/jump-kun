ba0254b332259920074e04cc734aa60ff0d0f449
TakaakiFuruse <takaakifuruse@gmail.com>
2019-06-24 23:05:46 +0900

    WIP Added log all dirs

--- src/dir_check.rs
+++ src/dir_check.rs
@@ -11,3 +11,11 @@ pub fn is_git_dir(entry: &DirEntry) -> bool {
 pub fn is_directory(entry: &DirEntry) -> bool {
     entry.file_type().is_dir()
 }
+
+pub fn is_not_hidden(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| entry.depth() == 0 || !s.starts_with("."))
+        .unwrap_or(false)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use jump_kun::history_to_hash::read_history;
 use jump_kun::{dir_up_down, select_item};
 extern crate skim;
+use dirs::home_dir;
+use jump_kun::dir_check;
 use jump_kun::dir_sorting::dir_string;
 use jump_kun::jump_then_add_to_hist::jump_then_add_to_hist;
 use jump_kun::structs::DirInfo;
 use skim::SkimOptionsBuilder;
 use std::collections::HashMap;
+use std::env;
+use std::fs::File;
+use std::io::BufWriter;
+use std::io::Write;
 use std::path::PathBuf;
+use walkdir::WalkDir;
 
 // shell script
 // function jump-kun-jump(){
@@ -15,6 +22,47 @@ use std::path::PathBuf;
 //         \cd $selected
 //         fi
 // }
+
+fn log_all_dirs() {
+    let mut h: HashMap<PathBuf, DirInfo> = HashMap::new();
+    WalkDir::new("/home/orz/")
+        .into_iter()
+        .filter_entry(|e| {
+            !dir_check::is_git_dir(e) && dir_check::is_directory(e) && dir_check::is_not_hidden(e)
+        })
+        .for_each(|x| {
+            if x.is_ok() {
+                h.insert(
+                    x.unwrap().path().to_path_buf(),
+                    DirInfo {
+                        cd_count: 1,
+                        from_history: true,
+                    },
+                );
+            };
+        });
+    WalkDir::new("/mnt/")
+        .into_iter()
+        .filter_entry(|e| {
+            !dir_check::is_git_dir(e) && dir_check::is_directory(e) && dir_check::is_not_hidden(e)
+        })
+        .for_each(|x| {
+            if x.is_ok() {
+                h.insert(
+                    x.unwrap().path().to_path_buf(),
+                    DirInfo {
+                        cd_count: 1,
+                        from_history: true,
+                    },
+                );
+            };
+        });
+    let mut history_dir: PathBuf = home_dir().unwrap();
+    history_dir.push(".config/jump-kun/history.log");
+    let mut log_file = BufWriter::new(File::create(history_dir).unwrap());
+    let b: &str = &serde_json::to_string(&h).unwrap();
+    let _ = log_file.write(&b.as_bytes());
+}
 
 pub fn main() {
     let options = SkimOptionsBuilder::default()
@@ -28,6 +76,9 @@ pub fn main() {
         .build()
         .unwrap();
 
+    if env::var("LOG_ALL_DIRS") == std::result::Result::Ok("true".to_string()) {
+        log_all_dirs();
+    }
     let history_hash: HashMap<PathBuf, DirInfo> = read_history();
     let mut found_dirs: HashMap<PathBuf, DirInfo> = dir_up_down::up_down();
 
