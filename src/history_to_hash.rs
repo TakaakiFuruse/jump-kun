3c99e86948a6bc695d757b7e2340fe449d988094
TakaakiFuruse <takaakifuruse@gmail.com>
2019-07-08 18:44:40 +0900

    added func

--- src/history_to_hash.rs
+++ src/history_to_hash.rs
@@ -16,7 +16,34 @@ pub fn read_history() -> HashMap<PathBuf, DirInfo> {
         Ok(e) => e,
         Err(_) => 0,
     };
-    let history_hash: HashMap<PathBuf, DirInfo> =
-        serde_json::from_str(&history).unwrap_or(HashMap::new());
+    let history_hash: HashMap<PathBuf, DirInfo> = history_hash(history);
     history_hash
+}
+
+fn history_hash(history: String) -> HashMap<PathBuf, DirInfo> {
+    let mut hm: HashMap<PathBuf, DirInfo> =
+        serde_json::from_str(&history).unwrap_or(HashMap::new());
+    hm.retain(|k, _| k.exists());
+    hm
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_history_hash_returns_empty_hashmap_if_dir_not_exist() {
+        let mut history: HashMap<PathBuf, DirInfo> = HashMap::new();
+        history.insert(PathBuf::from(r"\this\doesnt\exists"), DirInfo::new(1, true));
+        let result = history_hash(serde_json::to_string(&history).unwrap());
+        assert_eq!(result.is_empty(), true);
+    }
+
+    #[test]
+    fn test_history_hash_returns_empty_hashmap_if_dir_exists() {
+        let mut history: HashMap<PathBuf, DirInfo> = HashMap::new();
+        history.insert(dirs::home_dir().unwrap(), DirInfo::new(1, true));
+        let result = history_hash(serde_json::to_string(&history).unwrap());
+        assert_eq!(result.len(), 1);
+    }
 }
