25d6e6c5de960f6fbc6788f84df58e3ab61dd008
TakaakiFuruse <takaakifuruse@gmail.com>
2019-07-20 12:27:37 +0900

    added test, changed mod names, changed sort order

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use jump_kun::history_to_hash::read_history;
-use jump_kun::{dir_up_down, select_item};
+use jump_kun::{dir_finder, select_item};
 extern crate skim;
-use jump_kun::dir_sorting::dir_string;
+use jump_kun::dir_sorter;
 use jump_kun::jump_then_add_to_hist::jump_then_add_to_hist;
 use jump_kun::structs::DirInfo;
 use skim::SkimOptionsBuilder;
@@ -30,12 +30,12 @@ pub fn main() {
         .unwrap();
 
     let history_hash: HashMap<PathBuf, DirInfo> = read_history();
-    let mut found_dirs: HashMap<PathBuf, DirInfo> = dir_up_down::up_down();
+    let mut found_dirs: HashMap<PathBuf, DirInfo> = dir_finder::find_dirs();
 
     found_dirs.extend(history_hash.clone());
 
-    let all_dir_string = dir_string(found_dirs);
+    let all_dir_in_string = dir_sorter::to_sorted_string(found_dirs, dir_finder::current_dir());
 
-    let item = select_item::select(all_dir_string, &options);
+    let item = select_item::select(all_dir_in_string, &options);
     jump_then_add_to_hist(item, history_hash);
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,6 +1,8 @@
 use dirs::home_dir;
+use jump_kun::dir_sorter::to_sorted_string;
 use jump_kun::history_to_hash::history_hash;
 use jump_kun::structs::DirInfo;
+use jump_kun::walker::start_walking;
 use std::collections::HashMap;
 use std::path::PathBuf;
 
@@ -22,5 +24,67 @@ mod tests {
         history.insert(home_dir().unwrap(), DirInfo::new(1, true));
         let result = history_hash(serde_json::to_string(&history).unwrap());
         assert_eq!(result.len(), 1);
+    }
+
+    #[test]
+    fn test_start_walking_returns_hashmap() {
+        let result_hash = start_walking(home_dir().unwrap());
+        assert_eq!(result_hash.len() > 1, true);
+        for (_, v) in result_hash.iter() {
+            assert_eq!(v.cd_count, 0);
+            assert_eq!(v.from_history, false);
+        }
+    }
+
+    #[test]
+    fn test_start_walking_return_empty_hashmap_if_dir_not_exist() {
+        let result_hash = start_walking(PathBuf::from(r"\this\doesnt\exists"));
+        assert_eq!(result_hash.len(), 0);
+    }
+
+    #[test]
+    fn test_to_sorted_string_sort_order() {
+        // Current directory is on top.
+        // Sub directory of current will be next.
+        // Then sort by cd counts
+        let mut found_dirs = HashMap::new();
+        found_dirs.insert(
+            PathBuf::from(r"/test/1"),
+            DirInfo {
+                cd_count: 1,
+                from_history: false,
+            },
+        );
+        found_dirs.insert(
+            PathBuf::from(r"/test/2"),
+            DirInfo {
+                cd_count: 2,
+                from_history: false,
+            },
+        );
+        found_dirs.insert(
+            PathBuf::from(r"/test/3"),
+            DirInfo {
+                cd_count: 0,
+                from_history: false,
+            },
+        );
+        found_dirs.insert(
+            PathBuf::from(r"/sample/1"),
+            DirInfo {
+                cd_count: 0,
+                from_history: false,
+            },
+        );
+        found_dirs.insert(
+            PathBuf::from(r"/sample/2"),
+            DirInfo {
+                cd_count: 1,
+                from_history: false,
+            },
+        );
+        let current_directory = PathBuf::from("/test/3");
+        let result = to_sorted_string(found_dirs, current_directory);
+        assert_eq!(result, "/test/3\n/test/2\n/test/1\n/sample/2\n/sample/1\n");
     }
 }
