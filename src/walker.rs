6dfdc1320d76af37556332651982a96ad1a8b78d
TakaakiFuruse <takaakifuruse@gmail.com>
2019-09-21 17:01:45 +0900

    jump_kun_ignore added
    - now jump-kun reads ignore file and be able to ignore unnecessary files

--- src/walker.rs
+++ src/walker.rs
@@ -11,13 +11,13 @@ pub fn start_walking_around(from: Dir) -> DirVec {
             .min_depth(0)
             .max_depth(0)
             .into_iter()
-            .filter_entry(|e| !dir_check::is_git_dir(e) && dir_check::is_directory(e));
+            .filter_entry(|e| !dir_check::must_be_ignored(e) && dir_check::is_directory(e));
 
         let child_dirs = WalkDir::new(&dir)
             .min_depth(1)
             .max_depth(3)
             .into_iter()
-            .filter_entry(|e| !dir_check::is_git_dir(e) && dir_check::is_directory(e));
+            .filter_entry(|e| !dir_check::must_be_ignored(e) && dir_check::is_directory(e));
 
         for entry in parent_dirs {
             if entry.is_ok() {
