8eee568c0e5f419a3da7536c205cbc27ab84faab
TakaakiFuruse <takaakifuruse@gmail.com>
2020-03-05 18:30:03 +0900

    fixed skim API calls

--- src/select_item.rs
+++ src/select_item.rs
@@ -1,12 +1,16 @@
 use skim::{Skim, SkimOptions};
 use std::io::Cursor;
+extern crate skim;
+use skim::prelude::*;
 
 pub fn select(found_dirs: String, options: &SkimOptions) -> String {
-    let items = Skim::run_with(&options, Some(Box::new(Cursor::new(found_dirs))))
+    let item_reader = SkimItemReader::default();
+    let dirs = item_reader.of_bufread(Cursor::new(found_dirs));
+    let items = Skim::run_with(&options, Some(dirs))
         .map(|out| out.selected_items)
         .unwrap_or_else(|| Vec::new());
     if !items.is_empty() {
-        items[0].get_output_text().to_string()
+        items[0].text().to_string()
     } else {
         "".to_string()
     }
